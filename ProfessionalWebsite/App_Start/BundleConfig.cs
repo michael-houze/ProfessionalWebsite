7128d9e813fcf297aa885cd00146e38f806f73ac
Michael Houze <michael.houze@gmail.com>
2015-03-09 14:40:18 -0400

    Add style and script bundles

---
+++ ProfessionalWebsite/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Optimization;
+
+namespace ProfessionalWebsite
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Bundles/css")
+                        .Include("~/Content/bootstrap.css")
+                        .Include("~/Content/bootstrap-theme.css")
+                        .Include("~/Content/font-awesome.css")
+                        .Include("~/Content/jquery.smooth.scroll.css")
+                        .Include("~/Content/site.css")
+                        );
+
+            bundles.Add(new ScriptBundle("~/bundles/javascript")
+                        .Include("~/Scripts/jquery-2.1.3.js")        
+                        .Include("~/Scripts/bootstrap.js")
+                        .Include("~/Scripts/Chart.js")
+                        .Include("~/Scripts/jquery.smooth.scroll-1.0.1.js")
+                        );
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
